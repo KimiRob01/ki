ecb400cecd6b58760c8ced8cce2d252375b902b2
karna <kimi.rob01@gmail.com>
2016-04-05 12:30:00 +0200

    Trial_1

---
+++ app/src/main/java/app/kimi/camerarobot/constant/ExtraKey.java
@@ -0,0 +1,12 @@
+package app.kimi.camerarobot.constant;
+
+
+public class ExtraKey {
+    public static final String SETUP_PREFERENCE = "setup_preference";
+    public static final String QUALITY = "quality";
+    public static final String PREVIEW_SIZE = "preview_size";
+    public static final String OWN_PASSWORD = "own_password";
+    public static final String TARGET_PASSWORD = "target_password";
+    public static final String IP_ADDRESS = "ip_address";
+
+}
---
+++ app/src/main/java/app/kimi/camerarobot/utils/Utilities.java
@@ -0,0 +1,36 @@
+package app.kimi.camerarobot.utils;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+public class Utilities {
+    public static String getCurrentIP(Context context) {
+        WifiManager wifi = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        Method[] wmMethods = wifi.getClass().getDeclaredMethods();
+        for (Method method : wmMethods) {
+            if (method.getName().equals("isWifiApEnabled")) {
+
+                try {
+                    if (method.invoke(wifi).toString().equals("false")) {
+                        WifiInfo wifiInfo = wifi.getConnectionInfo();
+                        int ipAddress = wifiInfo.getIpAddress();
+                        return (ipAddress & 0xFF) + "." +
+                                ((ipAddress >> 8) & 0xFF) + "." +
+                                ((ipAddress >> 16) & 0xFF) + "." +
+                                ((ipAddress >> 24) & 0xFF);
+                    } else if (method.invoke(wifi).toString().equals("true")) {
+                        return "192.168.43.1";
+                    }
+                } catch (IllegalArgumentException | InvocationTargetException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return "Unknown";
+    }
+}
